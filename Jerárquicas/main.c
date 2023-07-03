229177b5f14ecfa143b919dbe9692b4c9ddf9451
Lota Schwager <101421100+LotaSchwager@users.noreply.github.com>
2023-07-02 22:50:03 -0400

    Agregando los nombres de los integrantes al main.c

--- Jerárquicas/main.c
+++ Jerárquicas/main.c
@@ -1,3 +1,10 @@
 #include "funciones.h"
+/*
+Integrantes:
+ -Ademir Muñoz 20.158.396-9
+ -Rodrigo Araos 21.154.061-3
+ -Fernando Guerrero 19.540.726-6
+*/
+
 //Se llama a la función "controlador" que esta en el archivo funciones.c
 int main(int argc, char *argv[]){controlador();return 0;}
